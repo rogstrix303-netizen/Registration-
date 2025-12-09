8341fd997de260bf58401cd640c9ed20c7ee7ce8
rogstrix303-netizen <rogstrix303@gmail.com>
2025-12-09 15:08:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("#regForm").on("submit", function () {
-    // Simple jQuery message before submit
-    $("#clientMsg").text("Submitting your application, please wait...");
-    return true; // allow form submit
-  });
-});
